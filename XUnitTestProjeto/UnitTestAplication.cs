6e6230cfd91ca633125d1313da5a4b8c6d72d554
elton naoki iwakura <eltoniwakura@tce.ms.gov.br>
2019-09-09 09:27:04 -0400

    envio de mais testes

--- XUnitTestProjeto/UnitTestAplication.cs
+++ XUnitTestProjeto/UnitTestAplication.cs
@@ -41,10 +41,16 @@ namespace XUnitTestProjeto
         [Theory]
         [InlineData(1,6)] //Pretendente abaixo de 30 anos e renda até 900
         [InlineData(2, 6)] //Pretendente acima de 45 anos e renda entre 901 e 1500
+        [InlineData(3, 8)] //Pretendente acima de 45 anos e renda até 900
+        [InlineData(4, 3)] //Pretendente acima de 45 anos e renda acima de 2000
+        [InlineData(5, 2)] //Pretendente abaixo de 30 anos e renda de 2000
+        [InlineData(6, 4)] //Pretendente abaixo de 30 anos e renda de 901 e dependente acima de 18
+        [InlineData(7, 6)] //Pretendente abaixo de 30 anos e renda de 901 e um dependente abaixo de 18
+        [InlineData(8, 7)] //Pretendente abaixo de 30 anos e renda de 901 e três dependentes abaixo de 18
         public void CalcularPontuacaoPorRenda(int idFamlia, int pontuacao)
         {
             //Arranjo
-            var familia = RetornaFamiliasParaCalcularPontosPorRenda().Where(p => p.Id == idFamlia).FirstOrDefault();
+            var familia = RetornaFamiliasParaCalcularPontos().FirstOrDefault(p => p.Id == idFamlia);
 
             //Ação
             var pontos = _familiaService.CalcularPontuacao(familia);
@@ -57,27 +63,81 @@ namespace XUnitTestProjeto
 
         #region MocksParaTeste
 
-        public List<Familia> RetornaFamiliasParaCalcularPontosPorRenda()
+        public List<Familia> RetornaFamiliasParaCalcularPontos()
         {
-            var familias = new List<Familia>();
-
-            familias.Add(new Familia {
-                Id = 1,
-                Rendas = new List<Renda> { new Renda { Valor = 500}, new Renda { Valor = 400 } },
-                Pessoas = new List<Pessoa> { new Pessoa { DataDeNascimento = new DateTime(2002,01,01),Tipo ="Pretendente" } },
-            });
-
-            familias.Add(new Familia
+            var familias = new List<Familia>
             {
-                Id = 2,
-                Rendas = new List<Renda> { new Renda { Valor = 900 }, new Renda { Valor = 10 } },
-                Pessoas = new List<Pessoa> { new Pessoa { DataDeNascimento = new DateTime(1968, 01, 01), Tipo = "Pretendente" } },
-            });
+                new Familia
+                {
+                    Id = 1,
+                    Rendas = new List<Renda> { new Renda { Valor = 500 }, new Renda { Valor = 400 } },
+                    Pessoas = new List<Pessoa> { new Pessoa { DataDeNascimento = new DateTime(2002, 01, 01), Tipo = "Pretendente" } },
+                },
+
+                new Familia
+                {
+                    Id = 2,
+                    Rendas = new List<Renda> { new Renda { Valor = 900 }, new Renda { Valor = 10 } },
+                    Pessoas = new List<Pessoa> { new Pessoa { DataDeNascimento = new DateTime(1968, 01, 01), Tipo = "Pretendente" } },
+                },
+
+                new Familia
+                {
+                    Id = 3,
+                    Rendas = new List<Renda> { new Renda { Valor = 200 }, new Renda { Valor = 300 } },
+                    Pessoas = new List<Pessoa> { new Pessoa { DataDeNascimento = new DateTime(1968, 01, 01), Tipo = "Pretendente" } },
+                },
+
+                new Familia
+                {
+                    Id = 4,
+                    Rendas = new List<Renda> { new Renda { Valor = 2000 }, new Renda { Valor = 5000 } },
+                    Pessoas = new List<Pessoa> { new Pessoa { DataDeNascimento = new DateTime(1968, 01, 01), Tipo = "Pretendente" } },
+                },
+
+                new Familia
+                {
+                    Id = 5,
+                    Rendas = new List<Renda> { new Renda { Valor = 300 }, new Renda { Valor = 1700 } },
+                    Pessoas = new List<Pessoa> { new Pessoa { DataDeNascimento = new DateTime(2002, 01, 01), Tipo = "Pretendente" } },
+                },
+
+                new Familia
+                {
+                    Id = 6,
+                    Rendas = new List<Renda> { new Renda { Valor = 600 }, new Renda { Valor = 301 } },
+                    Pessoas = new List<Pessoa> {
+                        new Pessoa { DataDeNascimento = new DateTime(1990, 01, 01), Tipo = "Pretendente" },
+                        new Pessoa { DataDeNascimento = new DateTime(2000, 01, 01), Tipo = "Dependente" }
+                    },
+                },
+                new Familia
+                {
+                    Id = 7,
+                    Rendas = new List<Renda> { new Renda { Valor = 600 }, new Renda { Valor = 301 } },
+                    Pessoas = new List<Pessoa> {
+                        new Pessoa { DataDeNascimento = new DateTime(1990, 01, 01), Tipo = "Pretendente" },
+                        new Pessoa { DataDeNascimento = new DateTime(2015, 01, 01), Tipo = "Dependente" },
+                        new Pessoa { DataDeNascimento = new DateTime(1991, 01, 01), Tipo = "Dependente" },
+                        new Pessoa { DataDeNascimento = new DateTime(1992, 01, 01), Tipo = "Dependente" },
+                    },
+                },
+                new Familia
+                {
+                    Id = 8,
+                    Rendas = new List<Renda> { new Renda { Valor = 600 }, new Renda { Valor = 301 } },
+                    Pessoas = new List<Pessoa> {
+                        new Pessoa { DataDeNascimento = new DateTime(1990, 01, 01), Tipo = "Pretendente" },
+                        new Pessoa { DataDeNascimento = new DateTime(2015, 01, 01), Tipo = "Dependente" },
+                        new Pessoa { DataDeNascimento = new DateTime(2014, 01, 01), Tipo = "Dependente" },
+                        new Pessoa { DataDeNascimento = new DateTime(2013, 01, 01), Tipo = "Dependente" },
+                    },
+                },
+            };
 
             return familias;
         }
 
         #endregion
-
     }
 }
