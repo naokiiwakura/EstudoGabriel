c7edfd4091ea2ebeb164efab28c5e55acbf62a51
elton naoki iwakura <eltoniwakura@tce.ms.gov.br>
2019-09-10 07:38:30 -0400

    ajuste

---
+++ Projeto/Controllers/SorteioController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProjetoAplication;
+using ProjetoDomain.Aplication;
+using ProjetoDomain.Dto;
+using ProjetoDomain.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace ProjetoProjeto.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SorteioController : ControllerBase
+    {
+        private readonly IFamiliaService _service;
+        private IMemoryCache _memoryCache;
+
+        public SorteioController(IFamiliaService service, IMemoryCache memoryCache)
+        {
+            _service = service;
+            _memoryCache = memoryCache;
+        }
+
+        public SorteioDto Get()
+        {
+            var familia = new SorteioDto();
+
+            if (!_memoryCache.TryGetValue("sorteio", out familia))
+            {
+                familia = _service.SortearFamilia();
+                MemoryCacheEntryOptions cacheOptions = new MemoryCacheEntryOptions();
+                cacheOptions.SetPriority(CacheItemPriority.High);
+                cacheOptions.AbsoluteExpiration = DateTime.Now.AddMinutes(10);
+                _memoryCache.Set("sorteio", familia, cacheOptions);
+            }
+
+            return familia;
+        }
+    }
+}
---
+++ ProjetoDomain/Dto/SorteioDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetoDomain.Dto
+{
+    public class SorteioDto
+    {
+        public int FamiliaId { get; set; }
+        public int QuantidadeDeCriterios { get; set; }
+        public int PontuacaoTotal { get; set; }
+        public DateTime DataSorteio { get; set; }
+    }
+}
